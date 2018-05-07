2a900866d2cb0563764685ac7e3d9b077531b35a
jatinderbhola <jatinderbhola@gmail.com>
2018-05-07 13:35:29 -0400

    Setup AWS authentication

--- back-end/amazon.js
+++ back-end/amazon.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3({
+    //signatureVersion: 'v4'
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_TOKEN
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
+router.post('/auth', function(req, res, next) {
+    console.log(req.body);
+    var temp_s3 = new AWS.S3({
+        //signatureVersion: 'v4'
+        accessKeyId: req.body.apiKey,
+        secretAccessKey: req.body.apiToken
+    });
+
+    var params = {};
+    temp_s3.listBuckets(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            res.status(500).send(err.message);
+        } // an error occurred
+        else {
+            console.log(data); // successful response
+            res.json(data);
+        }
+    });
+});
+
 
 module.exports = router;
--- front-end/controller/MainController.js
+++ front-end/controller/MainController.js
@@ -1,15 +1,41 @@
 angular.module('aws-sample')
 
-.controller('MainController', function($scope, $http) {
+.controller('MainController', function($scope, $http, $mdToast) {
     console.log('main controller calling');
+
+    $scope.toggleAuthContainer = false;
+    $scope.amazon = {};
     // authenticate the aws api token and key
     $scope.authentication = function() {
-        $http.get("/amazon").then((result) => {
-            console.log(result);
+        if (!$scope.amazon.apiKey) {
+            toastMsg("Enter api key! Required Field");
+            return;
+        }
+
+        if (!$scope.amazon.apiToken) {
+            toastMsg("Enter api Token! Required Field");
+            return;
+        }
+
+        $http.post("/amazon/auth", $scope.amazon).then((result) => { //wwww,x.com?a=a&b=b
+            result = result.data || result;
+            if (result.Buckets && result.Buckets.length)
+                toastMsg("Authenticated");
         }).catch((err) => {
-            console.error(result);
+            toastMsg(err.data)
+            console.error(err);
         });
     };
 
-    $scope.createBucket = function() {};
+    $scope.checkAuthRequest = function() {
+        $scope.toggleAuthContainer = true;
+    };
+
+    function toastMsg(msg) {
+        var toast = $mdToast.simple({ position: "bottom right" })
+            .textContent(msg)
+            .hideDelay(3000) //3000
+            .highlightAction(false);
+        $mdToast.show(toast);
+    }
 })
